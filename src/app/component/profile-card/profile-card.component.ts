2ed8e52648c0eab871379d6ccb0df22f90718776
hrociobc <hellen.pinel@unah.hn>
2026-01-21 13:58:49 -0600

    feat: navbar y perfil rediseñado

--- src/app/component/profile-card/profile-card.component.ts
+++ src/app/component/profile-card/profile-card.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
   styleUrls: ['./profile-card.component.css']
 })
-export class ProfileCardComponent {}
+export class ProfileCardComponent {
+ 
+  employeeData = {
+    number: '12345',
+    name: 'HELLEN ROCIO PINEL BACA',
+    center: 'CIUDAD UNIVERSITARIA',
+    imageUrl: 'assets/images/perfil.png'
+  };
+ 
+  pdfData = {
+    title: 'Instructivo Jefes',
+    size: '2.40 MB',
+    url: '#'
+  };
+ 
+  onViewCenso() {
+    console.log('Ver censo académico');
+  }
+ 
+  onViewCalificaciones() {
+    console.log('Ver calificaciones parciales');
+  }
+}
